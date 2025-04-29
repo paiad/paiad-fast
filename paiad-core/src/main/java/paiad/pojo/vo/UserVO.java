5578f7691293c571b9025ad491e79847b2f8e800
paiad <2369538173@qq.com>
2025-04-30 00:24:36 +0800

    docs: add UserVO.java

---
+++ paiad-core/src/main/java/paiad/pojo/vo/UserVO.java
@@ -0,0 +1,17 @@
+package paiad.pojo.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserVO {
+    private Long id;
+    private String username;
+    private String email;
+    private String phone;
+    private String role;
+    private String nickname;
+    private String avatar;
+    private LocalDateTime lastLoginTime;
+}
