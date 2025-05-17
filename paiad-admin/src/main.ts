4019a487e559df0b2127a58eb1a30b2a6398fc40
paiad <2369538173@qq.com>
2025-05-17 23:29:58 +0800

    docs: config internationalization

--- paiad-admin/src/main.ts
+++ paiad-admin/src/main.ts
@@ -3,8 +3,12 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+//国际化(zh)
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
