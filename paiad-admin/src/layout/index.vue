c30cbc9b33bbfaafaa7097faba84bc59b2126f0d
paiad <2369538173@qq.com>
2025-05-18 13:00:59 +0800

    docs: add Logo component

--- paiad-admin/src/layout/index.vue
+++ paiad-admin/src/layout/index.vue
@@ -6,7 +6,21 @@ import Logo from './logo/index.vue'
   <div class="layout-container">
     <!-- 左侧菜单 -->
     <div class="layout-slider">
-      <Logo></Logo>>
+      <Logo></Logo>
+      <!-- 展示菜单 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu background-color="#3f9fd0" text-color="#ffffff">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <el-sub-menu index="3">
+            <template #title>item four</template>
+            <el-menu-item index="2-1">用户管理</el-menu-item>
+            <el-menu-item index="2-2">角色管理</el-menu-item>
+            <el-menu-item index="2-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout-tabbar">111</div>
@@ -24,6 +38,10 @@ import Logo from './logo/index.vue'
       width: $base-menu-width;
       height: 100vh;
       background: $base-menu-background;
+      .scrollbar{
+          width: 100%;
+          height: calc(100vh - $base-menu-logo-height);
+      }
   }
   .layout-tabbar{
     position: fixed;
---
+++ paiad-admin/src/setting.ts
@@ -0,0 +1,7 @@
+// paiad-admin项目logo | 标题配置
+export default {
+  title: 'paiad-admin',
+  logo: '/logo.svg',
+  //是否隐藏图标
+  logoHidden: true,
+}
